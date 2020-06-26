97b04e568b3b8d2d91538c32a8404b071e791373
thatguy1155 <jamesglass@Jamess-MacBook-Pro.local>
2020-06-26 12:08:31 +0900

    first commit

---
+++ precache-manifest.d4c9f6b170fdb927784377445c6d05ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b75ce1e558b766648c497dc2b403acc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72874f4d70d169722b0",
+    "url": "/static/css/main.9550b40a.chunk.css"
+  },
+  {
+    "revision": "12b5d581e8cd4f447648",
+    "url": "/static/js/2.7aee1bf9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7aee1bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72874f4d70d169722b0",
+    "url": "/static/js/main.a30bc4ab.chunk.js"
+  },
+  {
+    "revision": "e4db7c41fb030e5ad0ee",
+    "url": "/static/js/runtime-main.ebb47c36.js"
+  }
+]);
---
+++ precache-manifest.efeb22a0384b54fcd657a9d2105808a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71ad12c02562d27012758026fc8318d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3df48810167f5908a2f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e05f8832c9b97cfe356",
+    "url": "/static/js/2.6831528c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6831528c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3df48810167f5908a2f",
+    "url": "/static/js/main.dbb0de91.chunk.js"
+  },
+  {
+    "revision": "e4db7c41fb030e5ad0ee",
+    "url": "/static/js/runtime-main.ebb47c36.js"
+  }
+]);
